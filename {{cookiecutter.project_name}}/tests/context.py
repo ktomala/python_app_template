53fd00888f4d7b9a2cfa0e25dde82ff51c3d833c
Karol Tomala <ktomala@golem.network>
2019-01-02 21:52:23 +0100

    Make sure pylint won't complain about unused import in tests after bootstrapping

--- {{cookiecutter.project_name}}/tests/context.py
+++ {{cookiecutter.project_name}}/tests/context.py
@@ -6,4 +6,4 @@ import sys
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
 
-import {{cookiecutter.project_name}}    # noqa
+import {{cookiecutter.project_name}}    # noqa pylint: disable=wrong-import-position,unused-import

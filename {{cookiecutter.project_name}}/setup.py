5e2949c7514b5f5c0c769a4b89be2337191b3100
Karol Tomala <karol.tomala@softwareag.com>
2020-09-29 12:36:03 +0200

    Add configurable entrypoint and script

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -9,8 +9,10 @@ from babel.messages import frontend as babel
 with open('README.md') as f:
     readme = f.read()
 
+{%- if cookiecutter.use_license == "yes" -%}
 with open('LICENSE.md') as f:
     license = f.read()
+{% endif %}
 
 setup(
     name='{{cookiecutter.project_name}}',
@@ -26,6 +28,13 @@ setup(
     license=license,
 {% endif %}
     packages=find_packages(exclude=('tests', 'docs')),
+{%- if cookiecutter.use_command_line_script == "yes" -%}
+    entry_points={
+        'console_scripts': [
+            '{{cookiecutter.script_name}}={{cookiecutter.project_name}}.{{cookiecutter.project_main}}:{{cookiecutter.project_main_entrypoint}}'
+        ]
+    },
+{% endif %}
     cmdclass={
         'compile_catalog': babel.compile_catalog,
         'extract_messages': babel.extract_messages,
---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/{{cookiecutter.project_main}}.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def {{cookiecutter.project_main_entrypoint}}():
+    pass
+
+
+if __name__ == '__main__':
+    {{cookiecutter.project_main_entrypoint}}()
